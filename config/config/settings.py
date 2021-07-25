5c5c134fd8f6996fb44bbc3467d95ca20cf917a8
sankarshvittal <sv6265@rit.edu>
2021-07-25 18:21:58 -0400

    update static_root in settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT = ''
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ( os.path.join('static'), )
